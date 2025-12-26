109c7e1aa61adbc23f8dffbfdbc0965cb7f23a13
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-26 11:13:53 -0300

    first commit

---
+++ src/main/java/io/github/aplaraujo/tdd_auth_empregados_exercicio/TddAuthEmpregadosExercicioApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.tdd_auth_empregados_exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAuthEmpregadosExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAuthEmpregadosExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aplaraujo/tdd_auth_empregados_exercicio/TddAuthEmpregadosExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo.tdd_auth_empregados_exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddAuthEmpregadosExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
